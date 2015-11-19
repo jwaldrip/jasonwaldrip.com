7cbe39023ab54bf468ce7f8398bc2d71c4497aee
Jason Waldrip <jason@waldrip.net>
2015-11-19 15:31:44 -0700

    add accomplishments

--- helpers/skills_helper.rb
+++ helpers/skills_helper.rb
@@ -1,7 +1,7 @@
 module SkillsHelper
 
   def languages
-    %w{ruby javascript crystal go bash python}
+    %w{ruby ecjavascript crystal go bash python terraform}
     # @skills ||= JSON.parse(Net::HTTP.get(URI.parse 'https://osrc.dfm.io/jwaldrip.json'))['usage']['languages'].map { |l| l['language'] }
   end
 
